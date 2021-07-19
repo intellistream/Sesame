c660b056ac94980be731e2267588f106fce0b0a4
Shuhao Zhang <tonyzhang19900609@gmail.com>
2021-07-19 18:48:25 +0800

    Major Structure Update. Able to run.
    Former-commit-id: ab9b76582f5a90553e0db3a449fce51c3a50dfe6

--- benchmark/include/util/BenchmarkUtils.hpp
+++ benchmark/include/util/BenchmarkUtils.hpp
@@ -13,14 +13,15 @@ struct param_t {
   int pointNumber;
   int clusterNumber;
   int dimension;
-  int coreSize;
+  int coresetSize;
   int seed;
 };
 
 class BenchmarkUtils {
  public:
   static void parseArgs(int argc, char **argv, param_t *cmd_params);
-  static void defaultParam( param_t *cmd_params);
+  static void defaultParam(param_t *cmd_params);
+  static void print_help(char *string);
 };
 
 #endif // ONLINEMLBENCHMARK_BENCHMARK_SRC_UTIL_BENCHMARKUTILS_HPP_
