a06205fa1a3669c4da084db9be5a0e4116cebf5c
tuidan <1009575361@qq.com>
2021-08-06 04:19:55 +0800

    fix bug

--- include/Algorithm/OfflineClustering/KMeans.hpp
+++ include/Algorithm/OfflineClustering/KMeans.hpp
@@ -8,6 +8,9 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <random>
+#include <algorithm>
+#include <cmath>
 #include <Algorithm/DataStructure/Point.hpp>
 #include <Algorithm/OfflineClustering/OfflineClustering.hpp>
 
@@ -15,19 +18,20 @@ namespace SESAME {
 class KMeans : public SESAME::OfflineClustering {
  public:
   // Randomly chooses k centres with kMeans++ distribution
-  std::vector<SESAME::PointPtr> chooseRandomCentres(int k, int n, std::vector<PointPtr> &points);
-
-  // kMeans++ algorithm for n points of dimension d with k centres
-  std::vector<SESAME::PointPtr> lloydPlusPlus(int k, int n, int d, std::vector<PointPtr> &points, double *resultCost);
-
-  // computes the target function for the given pointarray points[] (of size n) with the given array of centres centres[] (of size k)
-  double targetFunctionValue(int k, int n, std::vector<PointPtr> &centres, std::vector<PointPtr> &points);
-
-  // Computes the index of the centre nearest to p with the given array of centres centres[] (of size k)
-  int determineClusterCentreKMeans(int k, PointPtr p, std::vector<PointPtr> &centres);
-
-  // Computes the cost of point p with centre centre
-  double costOfPointToCenter(PointPtr p, PointPtr centre);
+  double calculateEluDistance(PointPtr &point, PointPtr &center);
+  void calculateClusterCenter(PointPtr &center,std::vector<PointPtr> &group);
+  void randomSelectCenters(int numberOfCenters, int numberOfInput,
+                           std::vector<PointPtr> &input, std::vector<PointPtr> &centers);
+  void selectCentersFromWeight(int numberOfCenters, int numberOfInput,
+                               std::vector<PointPtr> &input, std::vector<PointPtr> &centers);
+  void groupPointsByCenters(int numberOfCenters, int numberOfInput,
+                            std::vector<PointPtr> &input, std::vector<PointPtr> &centers,
+                            std::vector<std::vector<PointPtr>> &groups);
+  void adjustClusteringCenters(std::vector<PointPtr> &centers,std::vector<vector<PointPtr>> &groups);
+  void refreshGroup(std::vector<std::vector<PointPtr>> &oldGroups, vector<vector<PointPtr>> &newGroups);
+  void checkStopStatus(bool &flag,std::vector<std::vector<PointPtr>> &oldGroups,std::vector<std::vector<PointPtr>> &newGroups);
+  void storeResult(std::vector<std::vector<PointPtr>> &groups, std::vector<PointPtr> &output);
+  void runKMeans(int numberOfCenters, int numberOfInput, std::vector<PointPtr> &input, std::vector<PointPtr> &output, bool KMeansPP);
 };
 }
 #endif //SESAME_INCLUDE_ALGORITHM_OFFLINECLUSTERING_KMEANS_HPP_
