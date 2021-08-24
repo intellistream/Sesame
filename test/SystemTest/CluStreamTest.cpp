c46b94c6071d85e1d381c070aff718e2e55d1fa3
tuidan <1009575361@qq.com>
2021-08-24 16:56:11 +0800

    revise Gtest

---
+++ test/SystemTest/CluStreamTest.cpp
@@ -0,0 +1,52 @@
+// Copyright (C) 2021 by the IntelliStream team (https://github.com/intellistream)
+
+//
+// Created by shuhao zhang on 8/8/2021.
+//
+#include <filesystem>
+#include <gtest/gtest.h>
+#include <Utils/BenchmarkUtils.hpp>
+#include <Utils/Logger.hpp>
+#include <Sources/DataSourceFactory.hpp>
+#include <Sinks/DataSinkFactory.hpp>
+#include <Algorithm/AlgorithmFactory.hpp>
+
+TEST(SystemTest, CluStreamTest) {
+  //Setup Logs.
+  setupLogging("benchmark.log", LOG_DEBUG);
+
+  //Parse parameters.
+  param_t cmd_params;
+  cmd_params.pointNumber = 15120;
+  cmd_params.seed = 10;
+  cmd_params.clusterNumber = 10;
+  cmd_params.dimension = 54;
+  cmd_params.coresetSize = 100;
+  cmd_params.lastArrivingNum = 60;
+  cmd_params.timeWindow = 6;
+  cmd_params.timeInterval = 4;
+  cmd_params.onlineClusterNumber = 15;
+  cmd_params.radiusFactor = 70;
+  cmd_params.initBuffer = 500;
+  cmd_params.offlineTimeWindow = 2;
+  cmd_params.inputPath = std::filesystem::current_path().generic_string() + "/datasets/CoverType.txt";
+  cmd_params.outputPath = "results.txt";
+  cmd_params.algoName = "CluStream";//StreamKMeans  CluStream
+
+  std::vector<SESAME::PointPtr> input;
+  std::vector<SESAME::PointPtr> results;
+
+  //Create Spout.
+  SESAME::DataSourcePtr sourcePtr = SESAME::DataSourceFactory::create();
+  //Directly load data from file. TODO: configure it to load from external sensors, e.g., HTTP.
+  BenchmarkUtils::loadData(cmd_params, sourcePtr);
+
+  //Create Sink.
+  SESAME::DataSinkPtr sinkPtr = SESAME::DataSinkFactory::create();
+
+  //Create Algorithm.
+  SESAME::AlgorithmPtr algoPtr = SESAME::AlgorithmFactory::create(cmd_params);
+
+  //Run algorithm producing results.
+  BenchmarkUtils::runBenchmark(cmd_params, sourcePtr, sinkPtr, algoPtr);
+}
---
+++ test/SystemTest/StreamKMTest.cpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2021 by the IntelliStream team (https://github.com/intellistream)
+
+//
+// Created by shuhao zhang on 8/8/2021.
+//
+#include <filesystem>
+#include <gtest/gtest.h>
+#include <Utils/BenchmarkUtils.hpp>
+#include <Utils/Logger.hpp>
+#include <Sources/DataSourceFactory.hpp>
+#include <Sinks/DataSinkFactory.hpp>
+#include <Algorithm/AlgorithmFactory.hpp>
+
+TEST(SystemTest, StreamKMTest) {
+  //Setup Logs.
+  setupLogging("benchmark.log", LOG_DEBUG);
+
+  //Parse parameters.
+  param_t cmd_params;
+  cmd_params.pointNumber = 150;
+  cmd_params.seed = 10;
+  cmd_params.clusterNumber = 10;
+  cmd_params.dimension = 4;
+  cmd_params.coresetSize = 30;
+
+  cmd_params.inputPath = std::filesystem::current_path().generic_string() + "/datasets/Mock.txt";
+  SESAME_INFO("Default Input Data Directory: " + cmd_params.inputPath);
+  cmd_params.outputPath = "results.txt";
+  cmd_params.algoName = "StreamKMeans";
+
+
+  std::vector<SESAME::PointPtr> input;
+  std::vector<SESAME::PointPtr> results;
+
+  //Create Spout.
+  SESAME::DataSourcePtr sourcePtr = SESAME::DataSourceFactory::create();
+  //Directly load data from file. TODO: configure it to load from external sensors, e.g., HTTP.
+  BenchmarkUtils::loadData(cmd_params, sourcePtr);
+
+  //Create Sink.
+  SESAME::DataSinkPtr sinkPtr = SESAME::DataSinkFactory::create();
+
+  //Create Algorithm.
+  SESAME::AlgorithmPtr algoPtr = SESAME::AlgorithmFactory::create(cmd_params);
+
+  //Run algorithm producing results.
+  BenchmarkUtils::runBenchmark(cmd_params, sourcePtr, sinkPtr, algoPtr);
+}
