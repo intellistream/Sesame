4782e3fa2eb7dc8a9312221f721d4c037c2a38d3
tuidan <1009575361@qq.com>
2021-08-25 03:19:29 +0800

    add system GTest for Birch

---
+++ test/SystemTest/BirchTest.cpp
@@ -0,0 +1,47 @@
+//
+// Created by tuidan on 2021/8/25.
+//
+
+
+#include <filesystem>
+#include <gtest/gtest.h>
+#include <Utils/BenchmarkUtils.hpp>
+#include <Utils/Logger.hpp>
+#include <Sources/DataSourceFactory.hpp>
+#include <Sinks/DataSinkFactory.hpp>
+#include <Algorithm/AlgorithmFactory.hpp>
+
+TEST(SystemTest, BirchTest) {
+  //Setup Logs.
+  setupLogging("benchmark.log", LOG_DEBUG);
+
+  //Parse parameters.
+  param_t cmd_params;
+  cmd_params.pointNumber = 150;
+  cmd_params.thresholdDistance = 10;
+  cmd_params.maxInternalNodes = 3;
+  cmd_params.maxLeafNodes = 3;
+  cmd_params.dimension = 4;
+
+  cmd_params.inputPath = std::filesystem::current_path().generic_string() + "/datasets/Mock.txt";
+  cmd_params.outputPath = "results.txt";
+  cmd_params.algoName = "Birch";
+
+
+  std::vector<SESAME::PointPtr> input;
+  std::vector<SESAME::PointPtr> results;
+
+  //Create Spout.
+  SESAME::DataSourcePtr sourcePtr = SESAME::DataSourceFactory::create();
+  //Directly load data from file. TODO: configure it to load from external sensors, e.g., HTTP.
+  BenchmarkUtils::loadData(cmd_params, sourcePtr);
+
+  //Create Sink.
+  SESAME::DataSinkPtr sinkPtr = SESAME::DataSinkFactory::create();
+
+  //Create Algorithm.
+  SESAME::AlgorithmPtr algoPtr = SESAME::AlgorithmFactory::create(cmd_params);
+
+  //Run algorithm producing results.
+  BenchmarkUtils::runBenchmark(cmd_params, sourcePtr, sinkPtr, algoPtr);
+}
