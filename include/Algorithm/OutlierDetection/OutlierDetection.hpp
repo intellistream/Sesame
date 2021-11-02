54be31bab7cb83cfa37d9ed60bf6ca94b463f6bd
Gabriel WU <45706792+GabrielWuNR@users.noreply.github.com>
2021-11-02 18:22:35 +0800

    Add Outlier Detection Part (#72)
    Former-commit-id: 1b4f4e1bfa2b3401daaec13b6608c6855077a66e

---
+++ include/Algorithm/OutlierDetection/OutlierDetection.hpp
@@ -0,0 +1,17 @@
+//
+// Created by 1124a on 2021/11/2.
+//
+
+#ifndef SESAME_INCLUDE_ALGORITHM_OUTLIERDETECTION_OUTLIERDETECTION_HPP_
+#define SESAME_INCLUDE_ALGORITHM_OUTLIERDETECTION_OUTLIERDETECTION_HPP_
+
+#include <memory>
+namespace SESAME {
+class OutlierDetection;
+typedef std::shared_ptr<OutlierDetection> OutlierDetectionPtr;
+
+class OutlierDetection {
+
+};
+}
+#endif //SESAME_INCLUDE_ALGORITHM_OUTLIERDETECTION_OUTLIERDETECTION_HPP_
