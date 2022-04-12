e742fb73443574386dffa51a27ad099be2753754
WangZhengru <wangshaun@outlook.com>
2022-04-13 02:26:16 +0800

    test: add GenericTest.V6

--- test/Generic/GenericTest.cpp
+++ test/Generic/GenericTest.cpp
@@ -255,3 +255,45 @@ TEST(GenericTest, V5) {
 
   ASSERT_NEAR(res->purity, 0.2987, 0.01);
 }
+
+TEST(GenericTest, V6) {
+  // Setup Logs.
+  setupLogging("benchmark.log", LOG_DEBUG);
+  // Parse parameters.
+  param_t cmd_params;
+  cmd_params.pointNumber = 3000;
+  cmd_params.thresholdDistance = 3000;
+  cmd_params.maxInternalNodes = 40;
+  cmd_params.maxLeafNodes = 20;
+  cmd_params.dimension = 54;
+  cmd_params.GTClusterNumber = 7;
+  cmd_params.timeDecay = false;
+  cmd_params.landmark = 1000;
+
+  cmd_params.inputPath = std::filesystem::current_path().generic_string() +
+                         "/datasets/CoverType.txt";
+  cmd_params.outputPath = "results.txt";
+  cmd_params.algoType = SESAME::V6Stream;
+
+  std::vector<SESAME::PointPtr> input;
+  std::vector<SESAME::PointPtr> results;
+
+  // Create Spout.
+  SESAME::DataSourcePtr sourcePtr = SESAME::DataSourceFactory::create();
+  // Directly load data from file. TODO: configure it to load from external
+  // sensors, e.g., HTTP.
+  BenchmarkUtils::loadData(cmd_params, sourcePtr);
+
+  // Create Sink.
+  SESAME::DataSinkPtr sinkPtr = SESAME::DataSinkFactory::create();
+
+  // Create Algorithm.
+  AlgorithmPtr algoPtr = GenericFactory::New<StreamClustering<
+      Landmark, ClusteringFeaturesTree, NoDetection, NoRefinement>>(cmd_params);
+
+  // Run algorithm producing results.
+  auto res =
+      BenchmarkUtils::runBenchmark(cmd_params, sourcePtr, sinkPtr, algoPtr);
+
+  ASSERT_NEAR(res->purity, 0.3987, 0.01);
+}
