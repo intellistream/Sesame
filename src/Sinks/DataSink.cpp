84d0c919eed81e664872ff108306399b91611ea1
WangZhengru <wangshaun@outlook.com>
2022-04-24 15:18:18 +0800

    feat: optimize DataSink with sleep

--- src/Sinks/DataSink.cpp
+++ src/Sinks/DataSink.cpp
@@ -1,39 +1,44 @@
-// Copyright (C) 2021 by the IntelliStream team (https://github.com/intellistream)
+// Copyright (C) 2021 by the IntelliStream team
+// (https://github.com/intellistream)
 
 //
 // Created by Shuhao Zhang on 20/07/2021.
 //
 
-#include <Sinks/DataSink.hpp>
+#include "Sinks/DataSink.hpp"
+
+#include <chrono>
+#include <thread>
+
+using namespace std::chrono_literals;
 
 SESAME::DataSink::DataSink() {
   outputQueue = std::make_shared<std::queue<PointPtr>>();
   threadPtr = std::make_shared<SingleThread>();
   sourceEnd = false;
 }
+
 void SESAME::DataSink::runningRoutine() {
   barrierPtr->arrive_and_wait();
   SESAME_INFO("DataSink start to grab data");
   while (!sourceEnd) {
-    if (!outputQueue->empty()) {
-      PointPtr result = outputQueue->front();
-      output.push_back(result);
+    while (!outputQueue->empty()) {
+      output.push_back(outputQueue->front());
       outputQueue->pop();
     }
+    std::this_thread::sleep_for(1ms);
   }
   while (!outputQueue->empty()) {
-    PointPtr result = outputQueue->front();
-    output.push_back(result);
+    output.push_back(outputQueue->front());
     outputQueue->pop();
   }
-  finished=true;
+  finished = true;
   barrierPtr->arrive_and_wait();
   SESAME_INFO("DataSink sourceEnd grab data, in total:" << output.size());
 }
+
 bool SESAME::DataSink::start(int id) {
-  auto fun = [this]() {
-    runningRoutine();
-  };
+  auto fun = [this]() { runningRoutine(); };
   threadPtr->construct(fun, id);
   SESAME_INFO("DataSink spawn thread=" << threadPtr->getID());
   return true;
@@ -45,26 +50,25 @@ bool SESAME::DataSink::stop() {
     threadPtr->join();
     threadPtr.reset();
   } else {
-    SESAME_INFO("DataSink " << ": Thread is not joinable" << threadPtr->getID());
+    SESAME_INFO("DataSink "
+                << ": Thread is not joinable" << threadPtr->getID());
     return false;
   }
   return true;
 }
+
 void SESAME::DataSink::put(SESAME::PointPtr resultPtr) {
   outputQueue->push(resultPtr);
 }
-std::vector<SESAME::PointPtr> SESAME::DataSink::getResults() {
-  return output;
-}
+
+std::vector<SESAME::PointPtr> SESAME::DataSink::getResults() { return output; }
+
 void SESAME::DataSink::setBarrier(SESAME::BarrierPtr barrierPtr) {
   this->barrierPtr = barrierPtr;
 }
-SESAME::DataSink::~DataSink() {
-  stop();
-}
-void SESAME::DataSink::Ended() {
-  this->sourceEnd = true;
-}
-bool SESAME::DataSink::isFinished() {
-  return this->finished.load();
-}
+
+SESAME::DataSink::~DataSink() { stop(); }
+
+void SESAME::DataSink::Ended() { this->sourceEnd = true; }
+
+bool SESAME::DataSink::isFinished() { return this->finished.load(); }
