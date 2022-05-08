4af75934fa0bf722e1e1c90d393f3bc76379b8ba
WangZhengru <wangshaun@outlook.com>
2022-05-08 20:29:29 +0800

    feat: add rest perf scripts

--- benchmark/scripts/perf-clustream.sh
+++ benchmark/scripts/perf-clustream.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/part.sh"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/all.sh"
 
 ticat ${meta} : mark-time bench.begin : ${cover} \
     : join.new algo 2 \
-    : join.new num_last_arr 2,5,8 \
-    : join.new time_window 200,275,350 \
-    : join.new num_online_clusters 80,110,140 \
+    : join.new num_last_arr 2 \
+    : join.new time_window 350 \
+    : join.new num_online_clusters 110 \
     : join.new radius 2,14,26 \
     : join.new buf_size 1500 \
     : join.run run.sesame
 
 ticat ${meta} : mark-time bench.begin : ${kdd99} \
     : join.new algo 2 \
-    : join.new num_last_arr 2,9,16 \
-    : join.new time_window 200,275,350 \
-    : join.new num_online_clusters 60,100,140 \
-    : join.new radius 2,5,8 \
+    : join.new num_last_arr 2 \
+    : join.new time_window 350 \
+    : join.new num_online_clusters 60 \
+    : join.new radius 8 \
     : join.new buf_size 1500 \
     : join.run run.sesame
 
 ticat ${meta} : mark-time bench.begin : ${sensor} \
     : join.new algo 2 \
-    : join.new num_last_arr 2,9,16 \
+    : join.new num_last_arr 2 \
     : join.new time_window 1000 \
-    : join.new num_online_clusters 100,150,200 \
+    : join.new num_online_clusters 200 \
     : join.new radius 15 \
     : join.new buf_size 500 \
     : join.run run.sesame
