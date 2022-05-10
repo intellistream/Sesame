996bc7734eeb42be6d266757a55db66ccaa882b2
WangZhengru <wangshaun@outlook.com>
2022-05-10 14:58:03 +0800

    fix: try CluStream with EDS input

--- benchmark/scripts/perf-slkmeans.sh
+++ benchmark/scripts/perf-slkmeans.sh
@@ -5,29 +5,29 @@
 ticat ${meta} : mark-time bench.begin : ${cover} \
     : join.new algo 7 \
     : join.new delta_grid 0.2 \
-    : join.new num_samples 1000 \
+    : join.new num_samples 100 \
     : join.run run.sesame
 
 ticat ${meta} : mark-time bench.begin : ${kdd99} \
     : join.new algo 7 \
     : join.new delta_grid 0.2 \
-    : join.new num_samples 1000 \
+    : join.new num_samples 100 \
     : join.run run.sesame
 
 ticat ${meta} : mark-time bench.begin : ${sensor} \
     : join.new algo 7 \
     : join.new delta_grid 0.2 \
-    : join.new num_samples 1000 \
+    : join.new num_samples 100 \
     : join.run run.sesame
 
 # ticat ${meta} : mark-time bench.begin : ${eds} \
 #     : join.new algo 7 \
 #     : join.new delta_grid 0.2 \
-#     : join.new num_samples 1000 \
+#     : join.new num_samples 100 \
 #     : join.run run.sesame
 
 # ticat ${meta} : mark-time bench.begin : ${edso} \
 #     : join.new algo 7 \
 #     : join.new delta_grid 0.2 \
-#     : join.new num_samples 1000 \
+#     : join.new num_samples 100 \
 #     : join.run run.sesame
