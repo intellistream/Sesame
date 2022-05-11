d7d7fdf9f6cadf304c91a66e55ed06084c2d305e
WangZhengru <wangshaun@outlook.com>
2022-05-11 12:53:55 +0800

    feat: update birch param

--- benchmark/scripts/birch.sh
+++ benchmark/scripts/birch.sh
@@ -4,35 +4,35 @@
 
 ticat ${meta} : mark-time bench_begin : ${cover} \
     : join.new algo 0 \
-    : join.new max_in_nodes [100,1000,300] \
-    : join.new max_leaf_nodes [100,1000,300] \
-    : join.new distance_threshold [100,1600,500] \
+    : join.new max_in_nodes [10,40,10] \
+    : join.new max_leaf_nodes [10,40,10] \
+    : join.new distance_threshold 100,200,300 \
     : join.run run.sesame
 
 ticat ${meta} : mark-time bench_begin : ${kdd99} \
     : join.new algo 0 \
-    : join.new max_in_nodes [100,1000,300] \
-    : join.new max_leaf_nodes [100,1000,300] \
-    : join.new distance_threshold [100,1600,500] \
+    : join.new max_in_nodes [10,40,10] \
+    : join.new max_leaf_nodes [10,40,10] \
+    : join.new distance_threshold 50,100,250,600 \
     : join.run run.sesame
 
 ticat ${meta} : mark-time bench_begin : ${sensor} \
     : join.new algo 0 \
-    : join.new max_in_nodes [100,1000,300] \
-    : join.new max_leaf_nodes [100,1000,300] \
-    : join.new distance_threshold [10,100,30] \
+    : join.new max_in_nodes [10,40,10] \
+    : join.new max_leaf_nodes [10,40,10] \
+    : join.new distance_threshold 10,20,30,40 \
     : join.run run.sesame
 
 ticat ${meta} : mark-time bench.begin : ${eds} \
     : join.new algo 0 \
-    : join.new max_in_nodes [100,1000,300] \
-    : join.new max_leaf_nodes [100,1000,300] \
-    : join.new distance_threshold [20,100,20] \
+    : join.new max_in_nodes [10,40,10] \
+    : join.new max_leaf_nodes [10,40,10] \
+    : join.new distance_threshold [10,100,10] \
     : join.run run.sesame
 
 ticat ${meta} : mark-time bench.begin : ${edso} \
     : join.new algo 0 \
-    : join.new max_in_nodes [100,1000,300] \
-    : join.new max_leaf_nodes [100,1000,300] \
-    : join.new distance_threshold [20,100,20] \
+    : join.new max_in_nodes [10,40,10] \
+    : join.new max_leaf_nodes [10,40,10] \
+    : join.new distance_threshold [10,100,10] \
     : join.run run.sesame
--- benchmark/scripts/create.sql
+++ benchmark/scripts/create.sql
@@ -51,38 +51,38 @@ CREATE TABLE IF NOT EXISTS sesame (
                 purity DOUBLE \
 );
 
-CREATE VIEW birch as SELECT id, bench_begin, algo, workload, max_in_nodes, max_leaf_nodes, distance_threshold, cmm, purity FROM sesame WHERE algo = 'Birch'; 
+CREATE VIEW birch as SELECT id, run_begin, algo, workload, num_points, max_in_nodes, max_leaf_nodes, distance_threshold, cmm, purity FROM sesame WHERE algo = 'Birch'; 
 
-CREATE VIEW streamkm as SELECT id, bench_begin, algo, tag, workload, seed, num_clusters, coreset_size, cmm, purity FROM sesame WHERE algo = 'StreamKMeans'; 
+CREATE VIEW streamkm as SELECT id, run_begin, algo, tag, workload, seed, num_clusters, coreset_size, cmm, purity FROM sesame WHERE algo = 'StreamKMeans'; 
 
-CREATE VIEW edmstream as SELECT id, bench_begin, algo, workload, radius, delta, beta, buf_size, alpha, lambda, cmm, purity FROM sesame WHERE algo = 'EDMStream'; 
+CREATE VIEW edmstream as SELECT id, run_begin, algo, workload, num_points, radius, delta, beta, buf_size, alpha, lambda, cmm, purity FROM sesame WHERE algo = 'EDMStream'; 
 
-CREATE VIEW dbstream as SELECT id, bench_begin, algo, workload, lambda, radius, clean_interval, min_weight, alpha, base, cmm, purity FROM sesame WHERE algo = 'DBStream'; 
+CREATE VIEW dbstream as SELECT id, run_begin, algo, workload, lambda, radius, clean_interval, min_weight, alpha, base, cmm, purity FROM sesame WHERE algo = 'DBStream'; 
 
-CREATE VIEW dstream as SELECT id, bench_begin, algo, workload, lambda, beta, cm, cl, grid_width, cmm, purity FROM sesame WHERE algo = 'DStream'; 
+CREATE VIEW dstream as SELECT id, run_begin, algo, workload, lambda, beta, cm, cl, grid_width, cmm, purity FROM sesame WHERE algo = 'DStream'; 
 
-CREATE VIEW denstream as SELECT id, bench_begin, algo, workload, num_points, buf_size, min_points, epsilon, base, lambda, mu, beta, cmm, purity FROM sesame WHERE algo = 'DenStream'; 
+CREATE VIEW denstream as SELECT id, run_begin, algo, workload, num_points, buf_size, min_points, epsilon, base, lambda, mu, beta, cmm, purity FROM sesame WHERE algo = 'DenStream'; 
 
-CREATE VIEW clustream as SELECT id, bench_begin, algo, workload, num_last_arr, time_window, num_online_clusters, radius, buf_size, cmm, purity FROM sesame WHERE algo = 'CluStream'; 
+CREATE VIEW clustream as SELECT id, run_begin, algo, workload, num_last_arr, time_window, num_online_clusters, radius, buf_size, cmm, purity FROM sesame WHERE algo = 'CluStream'; 
 
-CREATE VIEW slkmeans as SELECT id, bench_begin, algo, workload, num_points, delta_grid, num_samples, cmm, purity FROM sesame WHERE algo = 'SLKMeans'; 
+CREATE VIEW slkmeans as SELECT id, run_begin, algo, workload, num_points, delta_grid, num_samples, cmm, purity FROM sesame WHERE algo = 'SLKMeans'; 
 
-CREATE VIEW perf as SELECT id, bench_begin, algo, workload, num_points, timestampdiff(second, run_begin, run_end) as et, purity, cmm, qps, lat_us, sum_us/1000000 as sum_s, ds_us/1000000 as ds_s, out_us/1000000 as out_s, ref_us/1000000 as off_s FROM sesame WHERE id >= 6406;
+CREATE VIEW perf as SELECT id, run_begin, algo, workload, num_points, timestampdiff(second, run_begin, run_end) as et, purity, cmm, qps, lat_us, sum_us/1000000 as sum_s, ds_us/1000000 as ds_s, out_us/1000000 as out_s, ref_us/1000000 as off_s FROM sesame WHERE id >= 6406 AND num_points > 100000;
 
-CREATE VIEW g1 as SELECT id, bench_begin, algo, workload, landmark, outlier_distance_threshold as odt, outlier_cap as oc, seed, cmm, purity FROM sesame WHERE algo = 'G1'; 
+CREATE VIEW g1 as SELECT id, run_begin, algo, workload, num_points, landmark, outlier_distance_threshold as odt, outlier_cap as oc, seed, cmm, purity FROM sesame WHERE algo = 'G1'; 
 
-CREATE VIEW g2 as SELECT id, bench_begin, algo, workload, landmark, outlier_distance_threshold as odt, outlier_cap as oc, seed, cmm, purity FROM sesame WHERE algo = 'G2'; 
+CREATE VIEW g2 as SELECT id, run_begin, algo, workload, num_points, landmark, outlier_distance_threshold as odt, outlier_cap as oc, seed, cmm, purity FROM sesame WHERE algo = 'G2'; 
 
-CREATE VIEW g3 as SELECT id, bench_begin, algo, workload, landmark, outlier_distance_threshold as odt, outlier_cap as oc, seed, cmm, purity FROM sesame WHERE algo = 'G3'; 
+CREATE VIEW g3 as SELECT id, run_begin, algo, workload, num_points, landmark, outlier_distance_threshold as odt, outlier_cap as oc, seed, cmm, purity FROM sesame WHERE algo = 'G3'; 
 
-CREATE VIEW g4 as SELECT id, bench_begin, algo, workload, sliding, outlier_distance_threshold as odt, outlier_cap as oc, seed, cmm, purity FROM sesame WHERE algo = 'G4'; 
+CREATE VIEW g4 as SELECT id, run_begin, algo, workload, num_points, sliding, outlier_distance_threshold as odt, outlier_cap as oc, seed, cmm, purity FROM sesame WHERE algo = 'G4'; 
 
-CREATE VIEW g5 as SELECT id, bench_begin, algo, workload, num_points, outlier_distance_threshold as odt, outlier_cap as oc, lambda, alpha, cmm, purity FROM sesame WHERE algo = 'G5'; 
+CREATE VIEW g5 as SELECT id, run_begin, algo, workload, num_points, outlier_distance_threshold as odt, outlier_cap as oc, lambda, alpha, cmm, purity FROM sesame WHERE algo = 'G5'; 
 
-CREATE VIEW g6 as SELECT id, bench_begin, algo, workload, landmark, cmm, purity FROM sesame WHERE algo = 'G6'; 
+CREATE VIEW g6 as SELECT id, run_begin, algo, workload, num_points, landmark, cmm, purity FROM sesame WHERE algo = 'G6'; 
 
-CREATE VIEW g7 as SELECT id, bench_begin, algo, workload, landmark, outlier_density_threshold as odt, outlier_cap as oc, neighbor_distance as nd, cmm, purity FROM sesame WHERE algo = 'G7'; 
+CREATE VIEW g7 as SELECT id, run_begin, algo, workload, num_points, landmark, outlier_density_threshold as odt, outlier_cap as oc, neighbor_distance as nd, cmm, purity FROM sesame WHERE algo = 'G7'; 
 
-CREATE VIEW g8 as SELECT id, bench_begin, algo, workload, landmark, outlier_distance_threshold as odt, outlier_cap as oc, cmm, purity FROM sesame WHERE algo = 'G8'; 
+CREATE VIEW g8 as SELECT id, run_begin, algo, workload, num_points, landmark, outlier_distance_threshold as odt, outlier_cap as oc, cmm, purity FROM sesame WHERE algo = 'G8'; 
 
-CREATE VIEW g9 as SELECT id, bench_begin, algo, workload, landmark, outlier_distance_threshold as odt, outlier_cap as oc, beta, cm, cl, cmm, purity FROM sesame WHERE algo = 'G9'; 
+CREATE VIEW g9 as SELECT id, run_begin, algo, workload, num_points, landmark, outlier_distance_threshold as odt, outlier_cap as oc, beta, cm, cl, cmm, purity FROM sesame WHERE algo = 'G9'; 
