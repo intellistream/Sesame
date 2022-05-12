938290bf0b2ae931b32936652bb9cc9fab543ef5
WangZhengru <wangshaun@outlook.com>
2022-05-13 00:38:56 +0800

    feat: add edmstream & slkmeans script

--- benchmark/scripts/step-g3.sh
+++ benchmark/scripts/step-g3.sh
@@ -2,19 +2,19 @@
 
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/all.sh"
 
-for i in $(seq 1 5)
-do
-    eds=`eval echo \$\{eds$i\}`
-    ticat ${meta} : mark-time bench.begin : ${eds} \
-        : join.new algo 23 \
-        : join.new max_in_nodes 100 \
-        : join.new max_leaf_nodes 100 \
-        : join.new distance_threshold 100 \
-        : join.new landmark 1000 \
-        : join.new outlier_distance_threshold 15 \
-        : join.new outlier_cap 500 \
-        : join.run run.sesame
-done
+# for i in $(seq 1 5)
+# do
+#     eds=`eval echo \$\{eds$i\}`
+#     ticat ${meta} : mark-time bench.begin : ${eds} \
+#         : join.new algo 23 \
+#         : join.new max_in_nodes 100 \
+#         : join.new max_leaf_nodes 100 \
+#         : join.new distance_threshold 100 \
+#         : join.new landmark 1000 \
+#         : join.new outlier_distance_threshold 15 \
+#         : join.new outlier_cap 500 \
+#         : join.run run.sesame
+# done
 
 for i in $(seq 1 3)
 do
@@ -23,7 +23,7 @@ do
         : join.new algo 23 \
         : join.new max_in_nodes 100 \
         : join.new max_leaf_nodes 100 \
-        : join.new distance_threshold 20 \
+        : join.new distance_threshold 50 \
         : join.new landmark 1000 \
         : join.new outlier_distance_threshold 15 \
         : join.new outlier_cap 500 \
