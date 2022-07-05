5b79764461ab4575d54d4844400064691cad41f1
tuidan <1009575361@qq.com>
2022-07-05 13:07:02 +0800

    change Landmark from [20000,20000,10000]to [0,0,0] for G1,G3 and G8 on KDD

--- benchmark/scripts/kdd.sh
+++ benchmark/scripts/kdd.sh
@@ -76,7 +76,7 @@ ticat ${meta} : mark-time bench_begin : ${kdd99} \
     : join.new max_in_nodes 400 \
     : join.new max_leaf_nodes 100 \
     : join.new distance_threshold 600 \
-    : join.new landmark 20000 \
+    : join.new landmark 0 \
     : join.new outlier_distance_threshold 1000 \
     : join.new outlier_cap 100 \
     : join.run run.sesame
@@ -98,7 +98,7 @@ ticat ${meta} : mark-time bench_begin : ${kdd99} \
     : join.new max_in_nodes 400 \
     : join.new max_leaf_nodes 100 \
     : join.new distance_threshold 600 \
-    : join.new landmark 20000 \
+    : join.new landmark 0 \
     : join.new outlier_distance_threshold 1000 \
     : join.new outlier_cap 100 \
     : join.run run.sesame
@@ -146,7 +146,7 @@ ticat ${meta} : mark-time bench_begin : ${kdd99} \
 ticat ${meta} : mark-time bench_begin : ${kdd99} \
     : join.new algo 28 \
     : join.new distance_threshold 600 \
-    : join.new landmark 10000 \
+    : join.new landmark 0 \
     : join.new outlier_distance_threshold 1000 \
     : join.new outlier_cap 100 \
     : join.run run.sesame
