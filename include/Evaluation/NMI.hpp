82e65c11a8fb155392734191ef9c5b5e30c1646b
tuidan <1009575361@qq.com>
2023-01-03 03:02:58 -0500

    add nmi

---
+++ include/Evaluation/NMI.hpp
@@ -0,0 +1,17 @@
+//
+// Created by tuidan on 2023/1/3.
+//
+
+#ifndef SESAME_INCLUDE_EVALUATION_NMI_HPP_
+#define SESAME_INCLUDE_EVALUATION_NMI_HPP_
+#include <Algorithm/DataStructure/Point.hpp>
+#include <vector>
+namespace SESAME {
+
+class NMI {
+ public:
+  static double Evaluate(const std::vector<PointPtr> &inputs,
+                           const std::vector<PointPtr> &predicts, int gt_size, int predict_size);
+};
+}
+#endif //SESAME_INCLUDE_EVALUATION_NMI_HPP_
