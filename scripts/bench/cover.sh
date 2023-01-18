4156c19bc4015100ded23773145acffb5f0f3d65
wzru <wangshaun@outlook.com>
2023-01-18 15:56:39 +0800

    fix: bench data 235808

--- scripts/bench/cover.sh
+++ scripts/bench/cover.sh
@@ -2,76 +2,76 @@
 
 export cover="join.new input_file $HOME/Sesame/build/benchmark/datasets/CoverType.txt : join.new num_points 581012 : join.new dim 54 : join.new num_clusters 7 : join.new run_cmm false"
 
-ticat ${meta} : ${cover} \
-    : join.new algo 0 \
-    : join.new max_in_nodes 400 \
-    : join.new max_leaf_nodes 100 \
-    : join.new distance_threshold 600 \
-    : join.new landmark 10000000 \
-    : join.run run.sesame
+# ticat ${meta} : ${cover} \
+#     : join.new algo 0 \
+#     : join.new max_in_nodes 400 \
+#     : join.new max_leaf_nodes 100 \
+#     : join.new distance_threshold 600 \
+#     : join.new landmark 10000000 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${cover} \
-    : join.new algo 1 \
-    : join.new seed 1 \
-    : join.new coreset_size 7 \
-    : join.new k 70 \
-    : join.run run.sesame
+# ticat ${meta} : ${cover} \
+#     : join.new algo 1 \
+#     : join.new seed 1 \
+#     : join.new coreset_size 7 \
+#     : join.new k 70 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${cover} \
-    : join.new algo 2 \
-    : join.new num_last_arr 2 \
-    : join.new time_window 350 \
-    : join.new num_online_clusters 7 \
-    : join.new radius 14 \
-    : join.new buf_size 1500 \
-    : join.run run.sesame
+# ticat ${meta} : ${cover} \
+#     : join.new algo 2 \
+#     : join.new num_last_arr 2 \
+#     : join.new time_window 350 \
+#     : join.new num_online_clusters 7 \
+#     : join.new radius 14 \
+#     : join.new buf_size 1500 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${cover} \
-    : join.new algo 3 \
-    : join.new buf_size 500 \
-    : join.new min_points 30 \
-    : join.new epsilon 35 \
-    : join.new base 2 \
-    : join.new lambda 0.25 \
-    : join.new mu 6 \
-    : join.new beta 0.2 \
-    : join.run run.sesame
+# ticat ${meta} : ${cover} \
+#     : join.new algo 3 \
+#     : join.new buf_size 500 \
+#     : join.new min_points 30 \
+#     : join.new epsilon 35 \
+#     : join.new base 2 \
+#     : join.new lambda 0.25 \
+#     : join.new mu 6 \
+#     : join.new beta 0.2 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${cover} \
-    : join.new algo 4 \
-    : join.new lambda 0.125 \
-    : join.new radius 300 \
-    : join.new clean_interval 400 \
-    : join.new min_weight 0.5 \
-    : join.new alpha 0.5 \
-    : join.new base 2 \
-    : join.run run.sesame
+# ticat ${meta} : ${cover} \
+#     : join.new algo 4 \
+#     : join.new lambda 0.125 \
+#     : join.new radius 300 \
+#     : join.new clean_interval 400 \
+#     : join.new min_weight 0.5 \
+#     : join.new alpha 0.5 \
+#     : join.new base 2 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${cover} \
-    : join.new algo 5 \
-    : join.new radius 250 \
-    : join.new delta 1500 \
-    : join.new beta 1 \
-    : join.new buf_size 1000 \
-    : join.new alpha 0.998 \
-    : join.new lambda 1 \
-    : join.run run.sesame
+# ticat ${meta} : ${cover} \
+#     : join.new algo 5 \
+#     : join.new radius 250 \
+#     : join.new delta 1500 \
+#     : join.new beta 1 \
+#     : join.new buf_size 1000 \
+#     : join.new alpha 0.998 \
+#     : join.new lambda 1 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${cover} \
-    : join.new algo 6 \
-    : join.new lambda 0.998 \
-    : join.new beta 0.1 \
-    : join.new cm 5 \
-    : join.new cl 0.8 \
-    : join.new grid_width 50 \
-    : join.run run.sesame
+# ticat ${meta} : ${cover} \
+#     : join.new algo 6 \
+#     : join.new lambda 0.998 \
+#     : join.new beta 0.1 \
+#     : join.new cm 5 \
+#     : join.new cl 0.8 \
+#     : join.new grid_width 50 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${cover} \
-    : join.new algo 7 \
-    : join.new delta_grid 1 \
-    : join.new num_samples 100 \
-    : join.new sliding 100 \
-    : join.run run.sesame
+# ticat ${meta} : ${cover} \
+#     : join.new algo 7 \
+#     : join.new delta_grid 1 \
+#     : join.new num_samples 100 \
+#     : join.new sliding 100 \
+#     : join.run run.sesame
 
 ticat ${meta} : ${cover} \
     : join.new algo 21 \
@@ -191,16 +191,16 @@ ticat ${meta} : ${cover} \
 ticat ${meta} : ${cover} \
     : join.new algo 34 \
     : join.new delta_grid 1 \
-    : join.new num_samples 1 \
-    : join.new sliding 7 \
-    : join.new k 5000 \
-    : join.new landmark 7 \
+    : join.new num_samples 20 \
+    : join.new sliding 5000 \
+    : join.new k 7 \
+    : join.new landmark 100000 \
     : join.run run.sesame
 
 ticat ${meta} : ${cover} \
     : join.new algo 35 \
     : join.new seed 1 \
     : join.new landmark 300000 \
-    : join.new coreset_size 7 \
+    : join.new coreset_size 5 \
     : join.new outlier_cap 5 \
     : join.run run.sesame
--- scripts/bench/sensor.sh
+++ scripts/bench/sensor.sh
@@ -2,76 +2,76 @@
 
 export sensor="join.new input_file $HOME/Sesame/build/benchmark/datasets/sensor.txt : join.new num_points 2219803 : join.new dim 5 : join.new num_clusters 55 : join.new run_cmm false : join.new arr_rate 0"
 
-ticat ${meta} : ${sensor} \
-    : join.new algo 0 \
-    : join.new max_in_nodes 1000 \
-    : join.new max_leaf_nodes 1000 \
-    : join.new distance_threshold 500 \
-    : join.new landmark 10000000 \
-    : join.run run.sesame
+# ticat ${meta} : ${sensor} \
+#     : join.new algo 0 \
+#     : join.new max_in_nodes 1000 \
+#     : join.new max_leaf_nodes 1000 \
+#     : join.new distance_threshold 500 \
+#     : join.new landmark 10000000 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${sensor} \
-    : join.new algo 1 \
-    : join.new seed 100 \
-    : join.new coreset_size 55 \
-    : join.new k 1399 \
-    : join.run run.sesame
+# ticat ${meta} : ${sensor} \
+#     : join.new algo 1 \
+#     : join.new seed 100 \
+#     : join.new coreset_size 55 \
+#     : join.new k 1399 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${sensor} \
-    : join.new algo 2 \
-    : join.new num_last_arr 2 \
-    : join.new time_window 1000 \
-    : join.new num_online_clusters 20 \
-    : join.new radius 15 \
-    : join.new buf_size 1500 \
-    : join.run run.sesame
+# ticat ${meta} : ${sensor} \
+#     : join.new algo 2 \
+#     : join.new num_last_arr 2 \
+#     : join.new time_window 1000 \
+#     : join.new num_online_clusters 20 \
+#     : join.new radius 15 \
+#     : join.new buf_size 1500 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${sensor} \
-    : join.new algo 3 \
-    : join.new buf_size 500 \
-    : join.new min_points 5 \
-    : join.new epsilon 0.8 \
-    : join.new base 2 \
-    : join.new lambda 0.25 \
-    : join.new mu 6 \
-    : join.new beta 0.2 \
-    : join.run run.sesame
+# ticat ${meta} : ${sensor} \
+#     : join.new algo 3 \
+#     : join.new buf_size 500 \
+#     : join.new min_points 5 \
+#     : join.new epsilon 0.8 \
+#     : join.new base 2 \
+#     : join.new lambda 0.25 \
+#     : join.new mu 6 \
+#     : join.new beta 0.2 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${sensor} \
-    : join.new algo 4 \
-    : join.new lambda 0.01 \
-    : join.new radius 5 \
-    : join.new clean_interval 2000 \
-    : join.new min_weight 0.05 \
-    : join.new alpha 0.03 \
-    : join.new base 2 \
-    : join.run run.sesame
+# ticat ${meta} : ${sensor} \
+#     : join.new algo 4 \
+#     : join.new lambda 0.01 \
+#     : join.new radius 5 \
+#     : join.new clean_interval 2000 \
+#     : join.new min_weight 0.05 \
+#     : join.new alpha 0.03 \
+#     : join.new base 2 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${sensor} \
-    : join.new algo 5 \
-    : join.new radius 1.6 \
-    : join.new delta 300 \
-    : join.new beta 0.7 \
-    : join.new buf_size 10000 \
-    : join.new alpha 0.998 \
-    : join.new lambda 1 \
-    : join.run run.sesame
+# ticat ${meta} : ${sensor} \
+#     : join.new algo 5 \
+#     : join.new radius 1.6 \
+#     : join.new delta 300 \
+#     : join.new beta 0.7 \
+#     : join.new buf_size 10000 \
+#     : join.new alpha 0.998 \
+#     : join.new lambda 1 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${sensor} \
-    : join.new algo 6 \
-    : join.new lambda 0.998 \
-    : join.new beta 0.1 \
-    : join.new cm 3 \
-    : join.new cl 1 \
-    : join.new grid_width 200 \
-    : join.run run.sesame
+# ticat ${meta} : ${sensor} \
+#     : join.new algo 6 \
+#     : join.new lambda 0.998 \
+#     : join.new beta 0.1 \
+#     : join.new cm 3 \
+#     : join.new cl 1 \
+#     : join.new grid_width 200 \
+#     : join.run run.sesame
 
-ticat ${meta} : ${sensor} \
-    : join.new algo 7 \
-    : join.new delta_grid 1 \
-    : join.new num_samples 100 \
-    : join.new sliding 100 \
-    : join.run run.sesame
+# ticat ${meta} : ${sensor} \
+#     : join.new algo 7 \
+#     : join.new delta_grid 1 \
+#     : join.new num_samples 100 \
+#     : join.new sliding 100 \
+#     : join.run run.sesame
 
 ticat ${meta} : ${sensor} \
     : join.new algo 21 \
@@ -194,10 +194,10 @@ ticat ${meta} : ${sensor} \
 ticat ${meta} : ${sensor} \
     : join.new algo 34 \
     : join.new delta_grid 1 \
-    : join.new num_samples 6 \
-    : join.new sliding 44 \
-    : join.new landmark 264 \
-    : join.new k 10000 \
+    : join.new num_samples 500 \
+    : join.new sliding 200 \
+    : join.new landmark 1000000 \
+    : join.new k 5 \
     : join.run run.sesame
 
 ticat ${meta} : ${sensor} \
