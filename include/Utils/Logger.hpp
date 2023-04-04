c830000009c08b4013383c824539826d9c6cf3ef
WangZhengru <wangshaun@outlook.com>
2023-04-04 12:02:34 +0800

    feat: info ignore in release mode

--- include/Utils/Logger.hpp
+++ include/Utils/Logger.hpp
@@ -19,40 +19,6 @@ enum DebugLevel
     LOG_TRACE
 };
 
-// static void setupLogging(std::string logFile, DebugLevel level) {}
-
-// static std::string getDebugLevelAsString(DebugLevel level) {
-//   if (level == LOG_NONE) {
-//     return "LOG_NONE";
-//   } else if (level == LOG_WARNING) {
-//     return "LOG_WARNING";
-//   } else if (level == LOG_DEBUG) {
-//     return "LOG_DEBUG";
-//   } else if (level == LOG_INFO) {
-//     return "LOG_INFO";
-//   } else if (level == LOG_TRACE) {
-//     return "LOG_TRACE";
-//   } else {
-//     return "UNKNOWN";
-//   }
-// }
-
-// static DebugLevel getStringAsDebugLevel(std::string level) {
-//   if (level == "LOG_NONE") {
-//     return LOG_NONE;
-//   } else if (level == "LOG_WARNING") {
-//     return LOG_WARNING;
-//   } else if (level == "LOG_DEBUG") {
-//     return LOG_DEBUG;
-//   } else if (level == "LOG_INFO") {
-//     return LOG_INFO;
-//   } else if (level == "LOG_TRACE") {
-//     return LOG_TRACE;
-//   } else {
-//     throw std::runtime_error("Logger: Debug level unknown: " + level);
-//   }
-// }
-
 #define LEVEL_TRACE 6
 #define LEVEL_DEBUG 5
 #define LEVEL_INFO 4
@@ -63,11 +29,12 @@ enum DebugLevel
 #define SESAME_TRACE(TEXT) std::cerr << TEXT << std::endl;
 #ifndef NDEBUG
 #    define SESAME_DEBUG(TEXT) std::cerr << TEXT << std::endl;
+#    define SESAME_INFO(TEXT) std::cerr << TEXT << std::endl;
 #else
 #    define SESAME_DEBUG(TEXT) ;
+#    define SESAME_INFO(TEXT) ;
 #endif
 #define SESAME_TRACE(TEXT) std::cerr << TEXT << std::endl;
-#define SESAME_INFO(TEXT) std::cerr << TEXT << std::endl;
 #define SESAME_WARNING(TEXT) std::cerr << TEXT << std::endl;
 #define SESAME_ERROR(TEXT) std::cerr << TEXT << std::endl;
 #define SESAME_FATAL_ERROR(TEXT) std::cerr << TEXT << std::endl;
