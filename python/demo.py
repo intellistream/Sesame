a9b4fb3e3d8812906cebb5d02d002d0837ef58d8
Zhengru Wang <wangshaun@outlook.com>
2024-06-13 13:33:51 +0800

    feat: python wheels ci

--- python/demo.py
+++ python/demo.py
@@ -1,6 +1,6 @@
 #!python3
 
-from pysame import Benne, Birch
+from pysame import Benne, Birch, BenneObj
 # from sklearn.cluster import Birch
 
 X = [[0, 1], [0.3, 1], [-0.3, 1], [0, -1], [0.3, -1], [-0.3, -1]]
@@ -8,8 +8,15 @@ X = [[0, 1], [0.3, 1], [-0.3, 1], [0, -1], [0.3, -1], [-0.3, -1]]
 brc = Birch(
     n_clusters=2,
     dim=2,
-    threshold=0.5,
+    distance_threshold=0.5,
 )
 
 print(brc.partial_fit(X).predict(X))
-# print(brc)
+
+bne = Benne(
+    n_clusters=2,
+    dim=2,
+    distance_threshold=0.5,
+    obj=BenneObj.accuracy,
+)
+print(bne.partial_fit(X).predict(X))
